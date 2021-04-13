3f4ceb9f52a3edaa5019b2ba34e173b7d5f83c07
darukakaa <82357301+darukakaa@users.noreply.github.com>
2021-04-13 19:59:27 +0700

    Add files via upload

---
+++ PersonFile.java
@@ -0,0 +1,10 @@
+package personfile;
+
+public class PersonFile {
+  
+    public static void main(String[] args) {
+       PreferredCustomer preferredCustomer = new PreferredCustomer("Marshall","California State Route 85",133712,13374,true,500);
+       System.out.println(preferredCustomer.toString());
+    }
+    
+}
